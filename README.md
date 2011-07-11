10c5562684d79b40024159de7d9f1d0a9e00355c
Scott W. Bradley <scottwb@scottwb.net>
2011-07-11 15:33:55 -0700

    Initial vrsion of jQuery.saveButtonToggle

---
+++ README.md
@@ -0,0 +1,39 @@
+## Overview
+
+This is a simple jQuery plugin that disables the submit button in a form when any of the input fields annotated as required are blank, and enables it when all of them are non-blank. It also optionally disables the button after being clicked, while the form is presumably being submitted, and changes its text to a configurable 'pending' message.
+
+## Usage
+
+  * Add the `js_required` class to all input elements in the form that must have a non-blank value before the save button can be enabled. E.g.:
+```html
+<input type="text" class="js_required" name="username"/>
+```
+
+  * Add the `js_save` class to the submit/button element in the form that should be disabled when one or more of the required fields are blank. E.g.:
+```html
+<input type="submit" class="js_save" name="save" value="Save"/>
+```
+
+  * Optional: Add a `progress="..."` attribute to the submit/button element, and when the form is submitted, the button will become disabled and its text will change to the specified text. E.g.:
+```html
+<input type="submit" class="js_save" progress="Saving..." name="save" value="Save"/>
+```
+
+  * Add a javascript call to init a form with this plugin: E.g.:
+```javascript
+$(form_selector).saveButtonToggle();
+```
+
+## Note on Patches/Pull Requests
+ 
+* Fork the project.
+* Make your feature addition or bug fix.
+* Add tests for it. This is important so I don't break it in a
+  future version unintentionally.
+* Commit, do not mess with rakefile, version, or history.
+  (if you want to have your own version, that is fine but bump version in a commit by itself I can ignore when I pull)
+* Send me a pull request. Bonus points for topic branches.
+
+## Copyright
+
+Copyright (c) 2011 Scott W. Bradley. MIT License.
---
+++ jquery.saveButtonToggle.js
@@ -0,0 +1,130 @@
+/**  
+ * jQuery saveButtonToggle Plugin
+ * Version: 0.0.1
+ * URL: https://github.com/scottwb/jquery.saveButtonToggle
+ * Descripton: Enables/disables a form's save button based on the
+ *             required fields being filled in.
+ * Requires: jQuery
+ * Author: Scott W. Bradley (http://www.google.com/profiles/scottwb)
+ * Copyright: Copyright (c) 2011 Scott W. Bradley
+ * License: MIT
+ *
+ * Usage:
+ *
+ *   * Add the 'js_required' class to all input elements in the
+ *     form that must have a non-blank value before the save button
+ *     can be enabled. E.g.:
+ *
+ *       <input type="text" class="js_required" name="username"/>
+ *
+ *   * Add the 'js_save' class to the submit/button element in the
+ *     form that should be disabled when one or more of the required
+ *     fields are blank. E.g.:
+ *
+ *       <input type="submit" class="js_save" name="save" value="Save"/>
+ *
+ *   * Optional: Add a progress="..." attribute to the submit/button
+ *     element and when the form is submitted, the button will become
+ *     disabled and its text will change to the specified text. E.g.:
+ *
+ *       <input type="submit" class="js_save" progress="Saving..." .../>
+ *
+ *   * Add javascript call to init the a form with this plugin. E.g.:
+ *
+ *       $(form_selector).saveButtonToggle();
+ *
+ */
+(function($) {
+
+    ////////////////////////////////////////////////////////////
+    // Constants
+    ////////////////////////////////////////////////////////////
+    var PLUGIN_NAME = 'saveButtonToggle';
+
+
+    ////////////////////////////////////////////////////////////
+    // Private Methods
+    ////////////////////////////////////////////////////////////
+    function setSaveButtonState($this) {
+        var data     = $this.data(PLUGIN_NAME);
+        if (data.saveButton.attr('progress') == data.saveButton.val()) {
+            return;
+        }
+
+        var disabled = false;
+        data.requiredFields.each(function() {
+            var value = $(this).val();
+            if (!value || value.match(/^\s*$/)) {
+                disabled = true;
+            }
+        });
+        if (disabled) {
+            data.saveButton.attr('disabled', true);
+        }
+        else {
+            data.saveButton.removeAttr('disabled');
+        }
+    }
+
+
+    ////////////////////////////////////////////////////////////
+    // Public Methods
+    ////////////////////////////////////////////////////////////
+    var publicMethods = {
+        init: function(options) {return this.each(function() {
+            var $this = $(this);
+            var $requiredFields = $this.find('.js_required');
+            var $saveButton     = $this.find('.js_save');
+
+            var data  = $this.data(PLUGIN_NAME);
+            if (!data) {
+                $this.data(PLUGIN_NAME, {
+                    requiredFields : $requiredFields,
+                    saveButton     : $saveButton
+                });
+            }
+
+            $requiredFields.bind('keyup.' + PLUGIN_NAME, function() {
+                setSaveButtonState($this);
+            });
+
+            var progressText = $saveButton.attr('progress');
+            if (progressText) {
+                $this.bind('submit.' + PLUGIN_NAME, function() {
+                    $saveButton.attr('disabled', true);
+                    $saveButton.val(progressText);
+                    return true;
+                });
+            }
+
+            setSaveButtonState($this);
+        })},
+
+        destroy: function() {return this.each(function() {
+            var $this = $(this);
+            var data  = $this.data(PLUGIN_NAME);
+            $this.unbind('.' + PLUGIN_NAME);
+            data.requiredFields.unbind('.' + PLUGIN_NAME);
+            $this.removeData(PLUGIN_NAME);
+        })}
+    };
+
+
+    ////////////////////////////////////////////////////////////
+    // Plugin Initialization
+    ////////////////////////////////////////////////////////////
+    $.fn[PLUGIN_NAME] = function(method) {
+        if (publicMethods[method]) {
+            return publicMethods[method].apply(
+                this,
+                Array.prototype.slice.call(arguments, 1)
+            );
+        }
+        else if (typeof method == 'object' || !method) {
+            return publicMethods.init.apply(this, arguments);
+        }
+        else {
+            $.error('Method ' + method + ' does not exist on jQuery.' + PLUGIN_NAME);
+        }
+    };
+})(jQuery);
